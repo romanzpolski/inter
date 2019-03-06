824faf7b33d65e9e0fe827c6c7cab4226958a1fb
romanzpolski <romankondratczyk@gmail.com>
2019-03-06 21:18:59 +0000

    build to gh-pages

---
+++ precache-manifest.f7361b309b231a1da20ee51a07bdca42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "705d7031091532591c5a",
+    "url": "/static/css/main.63f898ac.chunk.css"
+  },
+  {
+    "revision": "705d7031091532591c5a",
+    "url": "/static/js/main.705d7031.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "61a49561f89388587250",
+    "url": "/static/css/2.4ef0ce8e.chunk.css"
+  },
+  {
+    "revision": "61a49561f89388587250",
+    "url": "/static/js/2.61a49561.chunk.js"
+  },
+  {
+    "revision": "1c63e615fca1e1ca0c74a1860b9068b1",
+    "url": "/index.html"
+  }
+];
